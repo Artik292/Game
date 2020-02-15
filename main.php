63da329ec45cc0da9d0565eb1c56305402d99047
Artur Konevnikov <artik292@gmail.com>
2020-02-15 12:27:25 +0200

    new update

--- main.php
+++ main.php
@@ -18,43 +18,15 @@ $a = 10;
   $col_3 = $columns->addColumn(3);
 
 
-    $val = $col_2->add(['FormField/Line', '2','disabled']);
+    $val = $col_2->add(['FormField/Line', '45']);
     //$val->addAction('Set Custom Value')->js('click', new \atk4\ui\jsReload($val, ['val' => $val->jsInput()->val(new \atk4\ui\jsExpression('parseInt([])+1', [$val->jsInput()->val()]), $val->jsInput()->focus())]));
 
 
   $clicker = $col_2->add(["Button","Click!!!","green fluid big"]);
-  $clicker->js('click', new \atk4\ui\jsReload($val, ['val' => $val->jsInput()->val(new \atk4\ui\jsExpression('parseInt([])+1', [$val->jsInput()->val()]), $val->jsInput()->focus())]));
-  //->js('click', $clicker->js()->text(new \atk4\ui\jsExpression('parseInt([])+1', [$clicker->js()->text()])));
-  /*$clicker->on('click', function ($m) use($clicker,$view) {
-    $_SESSION["i"] = $_SESSION["i"] + 1;
-    //return $view->js()->reload();
-    //return  new \atk4\ui\jsReload($view);
-    $clicker->js('true', new \atk4\ui\jsReload($view));
-    return $m;
-  });*/
-
-  /*$v = $col_2->add(['Button', 'ui' => 'segment'])->set(isset($_GET['val']) ? $_GET['val'] : 'No value');
-  $val = $col_2->add(['FormField/Line', '']);
-  $act = $val->addAction('Set Custom Value');
-  $act->on('click', function ($a) {
-    $_SESSION["i"] = $_SESSION["i"] + 1;
-    $a=20;
-    return "a";
-  });
-  $act->js('click', new \atk4\ui\jsReload($v, ['val' => $a]));*/
+  $clicker->js('click', new \atk4\ui\jsReload($val, ['val' => $val->jsInput()->val(new \atk4\ui\jsExpression('parseInt([])+1234', [$val->jsInput()->val()]), $val->jsInput()->focus())]));
 
 
-
-  //new \atk4\ui\jsReload($v, ['val' => $_SESSION["i"]], $val->jsInput()->focus()));
-
-
-
-
-
-
-
-
-
+/*
   function Plus($clicker,$model)
   {
 
@@ -68,6 +40,7 @@ $a = 10;
     return $clicker->js()->text(new \atk4\ui\jsExpression('parseInt([])+1', [$clicker->js()->text()]));
 
   }
+  */
 
   //$label = $col_2->add(["Button",$_SESSION['user_id']]);
 
@@ -76,9 +49,26 @@ $a = 10;
 
 
   $save->on('click', function ($m) use($val){
-    $_SESSION["i"] = $val->get();
+    $_SESSION["i"] = $val->jsInput()->get();
     return $m;
   });
 
 $exit = $app->add(['Button',"Exit","red"]);
 $exit->link(["exit"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$x2 = $col_3->add(["Button","click x2","massive inverted yellow","icon"=>"france flag"]);
+
+$pus = $col_3->add(["Button","+0.5 cli/sek",]);
