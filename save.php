57f5e98c6b788afe5fed6969afc8e7ed966547d6
artik292 <artik292@gmail.com>
2020-02-28 21:34:24 +0200

    Wooops

--- save.php
+++ save.php
@@ -4,6 +4,6 @@ require 'vendor/autoload.php';
 require 'connection.php';
 $save = new User($db);
 $save->load($_SESSION['user_id']);
-$a = $_GET['val'];
+$save['clicker_count'] = $_GET['val'];
 $save->save();
 echo "<script>window.close();</script>";
